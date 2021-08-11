835ad9e6f43d9ec85012797648295aaa8d36065f
Abdur rahman <abdur.rahman200301@gmail.com>
2021-08-11 12:26:45 +0600

    add data and style

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { server } from '../config'
 export default function Home({ articles }) {
    return (
       <div>
-         <ArticleList articles={articles} />
+         <ArticleList articles={articles} key={articles.id} />
       </div>
    )
 }
@@ -19,14 +19,3 @@ export const getStaticProps = async () => {
       },
    }
 }
-
-// export const getStaticProps = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-//   const articles = await res.json()
-
-//   return {
-//     props: {
-//       articles,
-//     },
-//   }
-// }
